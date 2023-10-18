0aabb50ab735e4bc84656c9236468a26a0e05241
valdersonjr <valdersonpontes@gmail.com>
2023-10-18 13:09:45 -0300

    1.4 - Criando o nosso APP

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AppProps } from "next/app";
+
+import Head from "next/head"
+
+import '../styles/globals.css'
+
+const MyApp:React.FC<AppProps> = ({ Component, pageProps  }) => {
+  return(
+    <React.Fragment>
+      <Head>
+        <title>OneBitGame</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home:React.FC = () => {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Home;
