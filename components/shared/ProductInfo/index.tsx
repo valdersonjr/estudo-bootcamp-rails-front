1f6c0cd2dc8d866d600afbef4f29fe88f57ab842
valdersonjr <valdersonpontes@gmail.com>
2023-11-22 10:06:07 -0300

    sf 3 add component product info

---
+++ components/shared/ProductInfo/index.tsx
@@ -0,0 +1,47 @@
+import { Button } from 'react-bootstrap';
+import styles from './styles.module.css';
+
+import { useRouter } from 'next/router';
+
+interface ProductInfoProps {
+  type?: string;
+}
+
+const ProductInfo: React.FC<ProductInfoProps> = ({ type = 'normal' }) => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.product}>
+      <div>
+        <img 
+          src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg"
+          alt="Product Game"
+          className={styles.image}
+        />
+      </div>
+
+      <div className={styles.product_details}>
+        <div>
+          <p>
+            God of War
+          </p>
+          <p>
+            Playstation 4
+          </p>
+        </div>
+
+        <div className={styles.button}>
+          <Button
+            className={
+              `${(type === 'highlighted' ? 'btn btn-info' : styles.normal_button)}`
+            }
+          > 
+            {`R$ 19,99`}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductInfo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import MainComponent from '../components/shared/MainComponent';
 import { useRouter  } from 'next/dist/client/router';
 
+import ProductInfo from '../components/shared/ProductInfo';
+
 
 const Home:React.FC = () => {
   const router = useRouter();
   
   return (
     <MainComponent>
-      <button  onClick={() => router.push('/Auth/Login')}>Login</button>
+      {/* <button  onClick={() => router.push('/Auth/Login')}>Login</button> */}
+      <h1>Home</h1>
+      <ProductInfo />
+      <ProductInfo type="highlighted" />
     </MainComponent>
   )
 }
