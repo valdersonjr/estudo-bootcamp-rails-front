63a953b3b90c921d4a39d651508d3bf5962118df
valdersonjr <valdersonpontes@gmail.com>
2023-11-22 13:20:12 -0300

    sf 13 ajustando nossa pesquisa no header

--- components/shared/Header/StorefrontHeader/index.tsx
+++ components/shared/Header/StorefrontHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 
@@ -11,8 +11,17 @@ import styles from './styles.module.css';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import Link from 'next/link';
 
+import ProductSearchService from '../../../../util/ProductSearchService';
+import { useRouter } from 'next/router';
+
 const StoreFrontHeader: React.FC = () => {
-    return(
+    const [search, setSearch] = useState('');
+    const router = useRouter();
+
+    const handleSearch = (): void => {
+        router.push(`/Search${ProductSearchService.execute({ search })}`);
+    }
+    return (
         <Row className={styles.background}>
             <Col md={6} className="mt-2">
                 <Logo />
@@ -21,20 +30,39 @@ const StoreFrontHeader: React.FC = () => {
             <Col md={6} className="mt-2 text-center">
                 <Row>
                     <Col md={6} className="mb-4 mb-md-0">
-                        <InputGroup>
-                            <FormControl placeholder="Pesquisar Produto" />
+                        <InputGroup className={`${router.pathname === '/Search' ? styles.hidden : ''}`}>
+                            <FormControl
+                                placeholder="Pesquisar produto"
+                                value={search}
+                                onChange={
+                                    (evt: React.ChangeEvent<HTMLInputElement>) =>
+                                        setSearch(evt.target.value)
+                                }
+
+                                onKeyUp={
+                                    (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                                        if (evt.key.toLowerCase() === 'enter') {
+                                            handleSearch();
+                                        }
+                                    }
+                                }
+                            />
                         </InputGroup>
                     </Col>
 
                     <Col md={6}>
                         <Row>
-                            <Col md={4} xs={4}>
-                                <FontAwesomeIcon icon={faSearch as IconProp} color='var(--color-gray-light)' />
+                            <Col className={`${router.pathname === '/Search' ? styles.hidden : ''}`}>
+                                <FontAwesomeIcon
+                                    icon={faSearch as IconProp}
+                                    color='var(--color-gray-light)'
+                                    onClick={handleSearch}
+                                />
                             </Col>
-                            <Col md={4} xs={4}>
+                            <Col>
                                 <FontAwesomeIcon icon={faShoppingCart as IconProp} color='var(--color-gray-light)' />
                             </Col>
-                            <Col md={4} xs={4}>
+                            <Col>
                                 <Link href="/Auth/Login">
                                     <a>
                                         <FontAwesomeIcon icon={faUserCircle as IconProp} color='var(--color-gray-light)' />
