50f885eb4bb6459bf699bbf8db122cc8346eb2a7
valdersonjr <valdersonpontes@gmail.com>
2023-11-23 20:25:49 -0300

    sf 15 ajustando pagina de pesquisa pt 2

--- services/home.ts
+++ services/home.ts
@@ -4,7 +4,6 @@ import HomeIndexData from '../dtos/HomeIndexData';
 
 const HomeService = {
   index: (url: string) => {
-    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', url);
     return api.get<HomeIndexData>(url).then(response => response.data);
   }
 }
