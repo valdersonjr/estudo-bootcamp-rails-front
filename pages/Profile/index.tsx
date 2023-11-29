1a632a0948c0eeaba0f4c9133df4d1b72c339814
valdersonjr <valdersonpontes@gmail.com>
2023-11-29 06:11:52 -0300

    sf 27 atalizando dados de usuario

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
 import BlueBackground from '../../components/shared/BlueBackground';
@@ -8,81 +9,167 @@ import Menu from '../../components/Storefront/Menu';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './styles.module.css';
+
+import withAuth from '../../components/withAuth';
+import { useSelector, useDispatch } from 'react-redux';
+import User from '../../dtos/User';
+import UsersService from '../../services/users';
+import { setLoggedUser } from '../../store/modules/auth/reducer';
+
+import { toast } from 'react-toastify';
 import StyledButton from '../../components/shared/SyledButton';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 const Profile: React.FC = () => {
-    return (
-        <MainComponent>
-            <Menu tab="personal_data" />
+  const user: User = useSelector((state:any) => state.auth.loggedUser);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
 
-            <Form className={styles.form}>
-                <BlueBackground>
-                    <div>
-                        <strong className="d-block">Leonardo Scorza</strong>
-                        <span className={styles.blue_text}>
-                            contato@onebitcode.com
-                        </span>
-                    </div>
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
-                    <Row className="mt-4">
-                        <Col xs={12}>
-                            <div>
-                                <span className="d-block">
-                                    Informações Pública
-                                </span>
-                                <small className={styles.blue_text}>
-                                    Essas informações serão exibidas publicamente
-                                </small>
+  const dispatch = useDispatch();
 
-                                <Form.Group className="p-4">
-                                    <Form.Label>Nome</Form.Label>
-                                    <Form.Control placeholder="Nome de exibição" className={styles.input_background} />
-                                </Form.Group>
-                            </div>
-                        </Col>
-                    </Row>
+  const handleFormSubmit = async (evt: React.FormEvent): Promise<void> => {
+    evt.preventDefault();
 
-                    <Row className="mb-4">
-                        <Col>
-                            <span className="d-block">
-                                Informações Pessoais
-                            </span>
-                            <small className={styles.blue_text}>
-                                Essas informações NÃO serão exibidas publicamente
-                            </small>
+    try {
+      await UsersService.update({
+        name, 
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+        id: user.id,
+        profile: user.profile
+      });
 
-                            <div className="pl-4 pr-4 pt-3">
-                                <Form.Group>
-                                    <Form.Label>E-mail</Form.Label>
-                                    <Form.Control placeholder="E-mail" className={styles.input_background} type="email" />
-                                </Form.Group>
+      toast.info('Usuário atualizado com sucesso!');
 
-                                <Form.Group>
-                                    <Form.Label>Senha</Form.Label>
-                                    <Form.Control placeholder="Senha" className={styles.input_background} type="password" />
-                                </Form.Group>
+      dispatch(setLoggedUser({
+        name,
+        email,
+        id: user.id,
+        profile: user.profile
+      }));
+    } catch (error) {
+      toast.error('Erro ao atualiar o usuário, tente novamente.');
+      console.log(error);
+    } finally {
+      setPassword('');
+      setPasswordConfirmation('');
+    }
+  }
 
-                                <Form.Group>
-                                    <Form.Label>Repetir Senha</Form.Label>
-                                    <Form.Control placeholder="Repetir Senha" className={styles.input_background} type="password" />
-                                </Form.Group>
-                            </div>
-                        </Col>
-                    </Row>
-                </BlueBackground>
+  return (
+    <MainComponent>
+      <Menu tab="personal_data"/>
 
-                <div className="float-right mt-4 mb-4">
-                    <StyledButton
-                        icon={faUser as IconProp}
-                        action="Salvar alterações"
-                        type_button="blue"
-                        type="submit"
-                    />
-                </div>
-            </Form>
-        </MainComponent>
-    );
+      <Form 
+        className={styles.form}
+        onSubmit={handleFormSubmit}
+      >
+        <BlueBackground>
+          <div>
+            <strong className="d-block">
+              {user.name}
+            </strong>
+            <span className={styles.blue_text}>
+              {user.email}
+            </span>
+          </div>
+
+          <Row className="mt-4">
+            <Col xs={12}>
+              <div>
+                <span className="d-block">
+                  Informações Pública
+                </span>
+                <small className={styles.blue_text}>
+                  Essas informações serão exibidas publicamente
+                </small>
+
+                <Form.Group className="p-4">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control 
+                    placeholder="Nome de exibição" 
+                    className={styles.input_background} 
+                    value={name}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setName(evt.target.value)
+                    }
+                  />
+                </Form.Group>
+              </div>
+            </Col>
+          </Row>
+
+          <Row className="mb-4">
+            <Col>
+              <span className="d-block">
+                Informações Pessoais
+              </span>
+              <small className={styles.blue_text}>
+                Essas informações NÃO serão exibidas publicamente
+              </small>
+
+              <div className="pl-4 pr-4 pt-3">
+                <Form.Group>
+                  <Form.Label>E-mail</Form.Label>
+                  <Form.Control 
+                    placeholder="E-mail" 
+                    className={styles.input_background}
+                    type="email"
+                    value={email}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setEmail(evt.target.value)
+                    }
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control 
+                    placeholder="Senha" 
+                    className={styles.input_background} 
+                    type="password"
+                    value={password}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setPassword(evt.target.value)
+                    }
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Repetir Senha</Form.Label>
+                  <Form.Control 
+                    placeholder="Repetir Senha" 
+                    className={styles.input_background} 
+                    type="password"
+                    value={passwordConfirmation}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setPasswordConfirmation(evt.target.value)
+                    }
+                  />
+                </Form.Group>
+              </div>
+            </Col>
+          </Row>
+        </BlueBackground>
+
+        <div className="float-right mt-4 mb-4">
+          <StyledButton 
+            icon={faUser} 
+            action="Salvar alterações" 
+            type_button="blue" 
+            type="submit" 
+          />
+        </div>
+      </Form>
+    </MainComponent>
+  );
 }
 
-export default Profile;
+export default withAuth(Profile);
