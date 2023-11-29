a2b14e0bc0eedf2a980e7b2c6b400df6a49b0610
valdersonjr <valdersonpontes@gmail.com>
2023-11-29 13:53:06 -0300

    sf 5 6 exibindo itens carrinho no cart modal

--- components/Storefront/CartModal/index.tsx
+++ components/Storefront/CartModal/index.tsx
@@ -1,6 +1,11 @@
 import styles from './styles.module.css';
 import CartModalItem from '../CartModalItem';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCartProduct } from '../../../store/modules/storefront/cartProducts/reducer';
+import ProductShow from '../../../dtos/ProductShow';
+import { useRouter } from 'next/router';
 import StyledButton from '../../shared/SyledButton';
 
 interface CartModalProps {
@@ -8,21 +13,37 @@ interface CartModalProps {
 }
 
 const CartModal: React.FC<CartModalProps> = ({ searchPage = false }) => {
+  const cartProducts: ProductShow[] = useSelector((state: any) => state.cartProducts);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleRemove = (index: number): void => {
+    dispatch(removeCartProduct(index));
+  }
+
   return (
     <div className={`${styles.container} ${searchPage ? styles.search_page : ''}`}>
       <div className={styles.items}>
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
+        {
+          cartProducts?.map(
+            (product, index) =>
+              <CartModalItem
+                key={index}
+                product={product}
+                handleRemove={() => handleRemove(index)}
+              />
+          )
+        }
+
       </div>
 
       <div className={styles.separator} />
 
       <div className={styles.actions}>
         <span>
-          R$ 999,99
+          {
+            `R$ ${cartProducts?.reduce((acc, item) => acc + item.price, 0).toFixed(2)}`
+          }
         </span>
 
         <StyledButton
@@ -30,6 +51,7 @@ const CartModal: React.FC<CartModalProps> = ({ searchPage = false }) => {
           action="Finalizar Compra"
           icon={faShoppingCart}
           className="btn btn-danger"
+          onClick={() => router.push('/Cart')}
         />
       </div>
     </div>
