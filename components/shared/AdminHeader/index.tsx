234c31e808e194458970add65ec797a5d7a2ab53
valdersonjr <valdersonpontes@gmail.com>
2023-11-29 06:15:22 -0300

    sf 28 add rotas profile 1

--- components/shared/AdminHeader/index.tsx
+++ components/shared/AdminHeader/index.tsx
@@ -1,18 +1,16 @@
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import styles from './styles.module.css';
-import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faUserCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';
 
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
 import User from '../../../dtos/User';
 import SignOutService from '../../../util/SignOutService';
-
-interface AdminHeaderProps {
-  name: string
-}
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 const AdminHeader: React.FC = () => {
   const router = useRouter();
@@ -21,60 +19,68 @@ const AdminHeader: React.FC = () => {
 
   return (
     <Row className={styles.background}>
-      <Col lg={6} xs={12} className={styles.menu}>
+      <Col xs={12} className={styles.menu}>
         <Link href="/Admin">
           <a>
             <FontAwesomeIcon
               icon={faSignal as IconProp}
               color="var(--color-gray-light)"
-              className={`ml-3 ${router.pathname === '/Admin' ? styles.active : ''}`} />
+              className={`ml-3 ${router.pathname === '/Admin' ? styles.active : ''}`}
+            />
           </a>
         </Link>
-
         <Link href="/Admin/Users/List">
           <a>
-            <FontAwesomeIcon icon={faUser as IconProp}
+            <FontAwesomeIcon
+              icon={faUser as IconProp}
               color="var(--color-gray-light)"
-              className={`ml-3 ${router.pathname === '/Admin/Users/List' ? styles.active : ''}`} />
+              className={`ml-3 ${router.pathname === '/Admin/Users/List' ? styles.active : ''}`}
+            />
           </a>
         </Link>
-
-
-
         <Link href="/Admin/Products/List">
-          <a><FontAwesomeIcon
-            icon={faGamepad as IconProp}
-            color="var(--color-gray-light)"
-            className={`ml-3 ${router.pathname === '/Admin/Products/List' ? styles.active : ''}`} />
+          <a>
+            <FontAwesomeIcon
+              icon={faGamepad as IconProp}
+              color="var(--color-gray-light)"
+              className={`ml-3 ${router.pathname === '/Admin/Products/List' ? styles.active : ''}`}
+            />
           </a>
         </Link>
         <Link href="/Admin/Categories/List">
-          <a><FontAwesomeIcon
-            icon={faCheckSquare as IconProp}
-            color="var(--color-gray-light)"
-            className={`ml-3 ${router.pathname === '/Admin/Categories/List' ? styles.active : ''}`} />
+          <a>
+            <FontAwesomeIcon
+              icon={faCheckSquare as IconProp}
+              color="var(--color-gray-light)"
+              className={`ml-3 ${router.pathname === '/Admin/Categories/List' ? styles.active : ''}`}
+            />
           </a>
         </Link>
         <Link href="/Admin/SystemRequirements/List">
-          <a><FontAwesomeIcon
-            icon={faLaptop as IconProp}
-            color="var(--color-gray-light)"
-            className={`ml-3 ${router.pathname === '/Admin/SystemRequirements/List' ? styles.active : ''}`} />
+          <a>
+            <FontAwesomeIcon
+              icon={faLaptop as IconProp}
+              color="var(--color-gray-light)"
+              className={`ml-3 ${router.pathname === '/Admin/SystemRequirements/List' ? styles.active : ''}`}
+            />
           </a>
         </Link>
         <Link href="/Admin/Coupons/List">
-          <a><FontAwesomeIcon
-            icon={faTicketAlt as IconProp}
-            color="var(--color-gray-light)"
-            className={`ml-3 ${router.pathname === '/Admin/Coupons/List' ? styles.active : ''}`} />
+          <a>
+            <FontAwesomeIcon
+              icon={faTicketAlt as IconProp}
+              color="var(--color-gray-light)"
+              className={`ml-3 ${router.pathname === '/Admin/Coupons/List' ? styles.active : ''}`}
+            />
           </a>
         </Link>
 
         <Link href="#">
-          <a><FontAwesomeIcon
-            icon={faDollarSign as IconProp}
-            color="var(--color-gray-light)"
-            className="ml-3" />
+          <a>
+            <FontAwesomeIcon
+              icon={faDollarSign as IconProp}
+              color="var(--color-gray-light)"
+              className="ml-3" />
           </a>
         </Link>
 
@@ -90,10 +96,12 @@ const AdminHeader: React.FC = () => {
           </a>
         </Link>
 
-      </Col>
-      <Col lg={6} xs={3} className={styles.profile}>
-        <span className={styles.name}>{name}</span>
-        <FontAwesomeIcon icon={faUserCircle as IconProp} color="var(--color-gray-light)" />
+        <Link href="/Profile">
+          <a className={styles.profile}>
+            <span className={styles.name}>{name}</span>
+            <FontAwesomeIcon icon={faUserCircle as IconProp} color="var(--color-gray-light)" />
+          </a>
+        </Link>
       </Col>
     </Row>
   )
